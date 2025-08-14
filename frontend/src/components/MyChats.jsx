3f7bfca4f6f2daaa0c4ae4531dcc7c0d20928600
Lokesh1933 <167898603+Lokesh1933@users.noreply.github.com>
2025-08-14 19:35:48 +0530

    min change

--- frontend/src/components/MyChats.jsx
+++ frontend/src/components/MyChats.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { ChatState } from '../Context/ChatProvider'
+import { useToast } from '@chakra-ui/react'
+import axios from 'axios'
 
 const MyChats = () => {
+  const [loggedUser,setLoggedUser] = useState()
   const { user, setSelectedChat, chats, setChats, notifications } = ChatState()
+  const toast  =useToast()
+  const fetchChats = async () => {
+    try {
+      const { data } = await axios.get('/api/chat', {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        }, //we can also use config here
+      })
+      setChats(data)
+    } catch (error) {
+      toast({
+        title: "Error fetching chats",
+        description: error.message,
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+        position: "bottom-left",
+      })
+    }
+  }
   return (
     <div>
       My Chats
